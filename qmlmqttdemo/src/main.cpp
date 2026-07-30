7faab6ff6425a3e8f210b76ed7e6ceeb5e172250
Tassos Tavlarides <tavlarides@gmail.com>
2026-07-30 14:32:20 +0300

    Refactor CMake configuration and implement QML module for MQTT demo

--- qmlmqttdemo/src/main.cpp
+++ qmlmqttdemo/src/main.cpp
@@ -12,20 +12,28 @@ int main(int argc, char *argv[]) {
   AppController controller;
 
   QQmlApplicationEngine engine;
-  engine.rootContext()->setContextProperty("appController", &controller);
-  engine.rootContext()->setContextProperty("messageModel", controller.messageModel());
+  // engine.rootContext()->setContextProperty("appController", &controller);
+  // engine.rootContext()->setContextProperty("messageModel", controller.messageModel());
 
-  const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
-  QObject::connect(
-      &engine,
-      &QQmlApplicationEngine::objectCreated,
-      &app,
-      [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-          QCoreApplication::exit(-1);
-      },
-      Qt::QueuedConnection);
+  engine.setInitialProperties({ { "appController", QVariant::fromValue(&controller) },
+                                { "messageModel", QVariant::fromValue(controller.messageModel()) } });
 
-  engine.load(url);
+  // const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+  engine.loadFromModule("DemoApp", "App");
+
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  // QObject::connect(
+  //     &engine,
+  //     &QQmlApplicationEngine::objectCreated,
+  //     &app,
+  //     [url](QObject *obj, const QUrl &objUrl) {
+  //       if (!obj && url == objUrl)
+  //         QCoreApplication::exit(-1);
+  //     },
+  //     Qt::QueuedConnection);
+
+  // engine.load(url);
   return app.exec();
 }
